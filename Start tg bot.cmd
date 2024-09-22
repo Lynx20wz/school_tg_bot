eb87d5faf320ead83e40d623e53078c20fea7dab
Lynx20wz <167802307+Lynx20wz@users.noreply.github.com>
2024-09-22 20:22:02 +0300

    Обновление файла быстрого запуска

--- Start tg bot.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Путь к вашему виртуальному окружению
-set VENV_PATH==\venv\Scripts\activate.bat
-
-:: Активация виртуального окружения
-call %VENV_PATH%
-
-:: Запуск вашего Python файла
-python main.py
